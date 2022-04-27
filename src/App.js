30afbe49069b0793bc7cd8163e49c48be223001d
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-04-27 08:56:51 -0500

    added the repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by{" "}
+        <a href="https://www.suzettesamm.io" target="_blank">
+          Suzette Samm
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/SuzetteSa/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
