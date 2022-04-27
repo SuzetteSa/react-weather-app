a51d7ba18dbcee23e54742a1e71db69bb6179e1f
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-04-27 09:10:35 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App"> <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was created by{" "}
@@ -17,6 +17,7 @@ export default function App() {
           open-sourced on GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
 }
