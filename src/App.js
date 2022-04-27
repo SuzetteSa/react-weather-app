289c3932e16ca0b8b83e6f1a6083e313bafc9fdb
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-04-27 09:17:46 -0500

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App"> <div className="container">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by{" "}
-        <a href="https://www.suzettesamm.io" target="_blank">
-          Suzette Samm
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/SuzetteSa/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+    <div className="App">
+      {" "}
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was created by{" "}
+          <a href="https://www.suzettesamm.io" target="_blank" rel="noreferrer">
+            Suzette Samm
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/SuzetteSa/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Weather(){
+ <div>Hello from Weather Component</div>
+}
