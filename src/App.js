48ad69dfdd218defe1501368721f191682a79cce
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-04-29 02:46:46 -0500

    added basic styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       {" "}
       <div className="container">
-        <h1>Weather App</h1>
+        
         <Weather />
         <footer>
           This project was created by{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,54 @@
 import React from "react";
+import "./Weather.css";
 
-export default function Weather(){
- <div>Hello from Weather Component</div>
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city....."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 11:00 AM</li>
+        <li>Mostly sunny</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="partly cloudy icon"
+              className="float-left"
+            />
+
+            <span className="temperature float-left">55</span>
+            <span className="unit float-left">°F</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 35%</li>
+            <li>Wind: 5mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
